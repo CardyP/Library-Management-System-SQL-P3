c9e55e74936689dc695818d6a40b91733e9ef729
Paolo Cardarella <Paolo.cardarella@gmail.com>
2025-11-07 14:19:58 -0500

    Add files via upload

---
+++ Library-System-Management-SQL-Database.sql
@@ -0,0 +1,88 @@
+-- Library Management System Project 2
+
+-- creating branch table
+CREATE DATABASE library_db
+
+DROP TABLE IF EXISTS branch;
+CREATE TABLE branch
+	(
+		branch_id VARCHAR(10) PRIMARY KEY,
+		manager_id VARCHAR(10),
+		branch_address VARCHAR(55),
+		contact_no VARCHAR(10)
+	);
+
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee
+	(
+		emp_id VARCHAR(25) PRIMARY KEY,
+		emp_name VARCHAR(25),
+		position VARCHAR(25),
+		salary FLOAT,
+		branch_id VARCHAR(25) -- FK
+
+	);
+
+
+DROP TABLE IF EXISTS books;
+CREATE TABLE books
+	(
+		isbn VARCHAR(20) PRIMARY KEY,
+		book_title VARCHAR(75),
+		category VARCHAR(25),
+		rental_price FLOAT,
+		status VARCHAR(25),
+		author VARCHAR(35),
+		publisher VARCHAR(35)
+	);
+
+
+DROP TABLE IF EXISTS members; 
+CREATE TABLE members
+	(
+		member_id VARCHAR(25) PRIMARY KEY,
+		member_name VARCHAR(25),
+		member_address VARCHAR(75),
+		reg_date DATE
+	);
+
+
+DROP TABLE IF EXISTS issued_status;
+CREATE TABLE issued_status
+	(
+		issued_id VARCHAR(25) PRIMARY KEY,
+		issued_member_id VARCHAR(25), -- FK
+		issued_book_name VARCHAR(75),
+		issued_date DATE,
+		issued_book_isbn VARCHAR(25), -- FK
+		issued_emp_id VARCHAR(25) -- FK
+	);
+
+
+DROP TABLE IF EXISTS return_status;
+CREATE TABLE return_status
+	(
+		return_id VARCHAR(25) PRIMARY KEY,
+		issued_id VARCHAR(25),
+		return_book_name VARCHAR(75),
+		return_date DATE,
+		return_book_isbn VARCHAR(25)
+	);
+
+-- FOREIGN KEY
+ALTER TABLE issued_status
+ADD CONSTRAINT fk_members
+FOREIGN KEY (issued_member_id)
+REFERENCES members(member_id);
+
+ALTER TABLE issued_status
+ADD CONSTRAINT fk_books
+FOREIGN KEY (issued_book_isbn)
+REFERENCES books(isbn);
+
+ALTER TABLE issued_status
+ADD CONSTRAINT fk_employees
+FOREIGN KEY (issued_emp_id)
+REFERENCES employee(emp_id)
+
